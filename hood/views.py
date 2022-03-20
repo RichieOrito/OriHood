d99a02bebc930405bb8f7e2db107c6f02b71c49d
RichieOrito <richard.omondi@student.moringaschool.com>
2022-03-20 20:19:48 +0300

    This commit adds the login and logout functions

--- hood/views.py
+++ hood/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
+
+def index(request):
+
+    return render(request, 'hood/index.html')
+
+def login_user():
+
+    return render(request, 'auth/login.html', context)
+
+def logout_user():
+    
+    return redirect('index')
