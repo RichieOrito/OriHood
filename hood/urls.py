4eab215c476119bf053edf56cb820abc5cddc58e
RichieOrito <richard.omondi@student.moringaschool.com>
2022-03-21 10:33:52 +0300

    This commit imports the project settings from the django.conf module and a static function

--- hood/urls.py
+++ hood/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path, include
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns =[
     path('', views.index, name='index'),
@@ -11,3 +13,5 @@ urlpatterns =[
     path('auth/register/', views.register_user, name='register'),
     path('accounts/logout/', views.logout_user, name='logout'),
 ]
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
