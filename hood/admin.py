59ab4342d05756e53a8af3437fd34c4a57c802ab
RichieOrito <richard.omondi@student.moringaschool.com>
2022-03-22 01:22:47 +0300

    This commit updates the admin

--- hood/admin.py
+++ hood/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Profile, Gender, Post, Hood, Business, PostType
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Gender)
+admin.site.register(Post)
+admin.site.register(Hood)
+admin.site.register(Business)
+admin.site.register(PostType)
